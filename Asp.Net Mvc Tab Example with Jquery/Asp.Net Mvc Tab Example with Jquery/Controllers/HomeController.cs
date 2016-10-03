fd8b3f4924074a2af63f5a4b71e901446d9afd73
necipbaser - Necip Başer <necipbaser71@gmail.com>
2016-10-03 17:11:49 +0300

    Project was created. Jquery code was added. Html code was added.

---
+++ Asp.Net Mvc Tab Example with Jquery/Asp.Net Mvc Tab Example with Jquery/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp.Net_Mvc_Tab_Example_with_Jquery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.Net Mvc Tab Example with Jquery/Asp.Net Mvc Tab Example with Jquery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Asp.Net_Mvc_Tab_Example_with_Jquery.MvcApplication" Language="C#" %>
